ccea61bcf17d59adf64aa98c3b928ef596288367
u174762 <sam.panga@hotmail.com>
2016-08-07 21:27:41 +0530

    Add files via upload

---
+++ ACD_JAVA_session2_Assignment3/src/assignment3.java
@@ -0,0 +1,32 @@
+
+public class assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		   for(int i=1; i<= 5 ;i++){
+               
+               for(int j=0; j < i; j++){
+                       System.out.print(j+1);
+               }
+              
+               //generate a new line
+               System.out.println("");
+       }
+      
+       //create second half of pyramid
+       for(int i=5; i>0 ;i--){
+              
+               for(int j=0; j < i; j++){
+                       System.out.print(j+1);
+               }
+              
+               //generate a new line
+               System.out.println("");
+       }
+
+}
+
+	}
+
+
